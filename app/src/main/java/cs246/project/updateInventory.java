3963ac4ec3dd0b7c325e258e97e57eb6724a1c98
kflet <47226151+kflet@users.noreply.github.com>
2019-03-09 22:09:33 -0700

    Kirk's UI Pages

---
+++ app/src/main/java/cs246/project/updateInventory.java
@@ -0,0 +1,15 @@
+package cs246.project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class updateInventory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_inventory);
+    }
+
+    // Test comment
+}
