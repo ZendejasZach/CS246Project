faed5cbedd3f69aca111597c42c9fa234b91a5a3
Zach Zendejas <ZAX2717@byui.edu>
2019-03-13 13:10:36 -0600

    Configured buttons to navigate to appropriate pages. Still need to test in emulator

--- app/src/main/java/cs246/project/startPage.java
+++ app/src/main/java/cs246/project/startPage.java
@@ -1,7 +1,10 @@
 package cs246.project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class startPage extends AppCompatActivity {
 
@@ -9,5 +12,56 @@ public class startPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_page);
+
+        // Buttons
+        // TODO: Test these methods in emulator.
+        viewInventoryButton();
+        updateInventoryButton();
+        settingsButton();
+        logout();
+    }
+
+    // Page not created yet
+    private void logout() {
+        Button logout = findViewById(R.id.logOut);
+        logout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+//                startActivity(new Intent(startPage.this, loginPage.class));
+//                Likely some other logic here to log the user out.
+            }
+        });
+    }
+
+    // Page not created yet
+    private void settingsButton() {
+        Button settings = findViewById(R.id.notificationSettings);
+        settings.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+//                startActivity(new Intent(startPage.this, notificationSettings.class));
+            }
+        });
+    }
+
+    private void updateInventoryButton() {
+        Button updateInventory = findViewById(R.id.updateInventory);
+        updateInventory.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(startPage.this, updateInventory.class));
+            }
+        });
+    }
+
+    // go to viewInventory activity
+    private void viewInventoryButton(){
+        Button viewInventory = findViewById(R.id.viewInventory);
+        viewInventory.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(startPage.this, viewInventory.class));
+            }
+        });
     }
 }
--- app/src/main/java/cs246/project/viewInventory.java
+++ app/src/main/java/cs246/project/viewInventory.java
@@ -2,6 +2,7 @@ package cs246.project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class viewInventory extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class viewInventory extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_inventory);
+
     }
 }
