6e5b1eb65ed9bcedc544f2f182d61b9d7a4ecb7e
kflet <47226151+kflet@users.noreply.github.com>
2019-03-13 19:08:01 -0600

    Kirk added log to UpdateInventory

--- app/src/main/java/cs246/project/updateInventory.java
+++ app/src/main/java/cs246/project/updateInventory.java
@@ -1,14 +1,33 @@
 package cs246.project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class updateInventory extends AppCompatActivity {
+
+    // logging
+    private static final String TAG = "updateInventory";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_inventory);
+    }
+
+    // go to scanAddition activity
+    private void scanAdditionButton(){
+        Button scanAddition = findViewById(R.id.scanAddition);
+        scanAddition.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Log.v(TAG, "Scan for Addition button tapped");
+                //startActivity(new Intent(updateInventory.this, scanAddition.class));
+            }
+        });
     }
 
     // Test comment
