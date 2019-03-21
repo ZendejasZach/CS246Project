46352c34befc77f5e22972b8ddc3ef47c3604feb
Zach <zen17001@byui.edu>
2019-03-20 18:43:06 -0600

    Created Javadoc for itemsNeeded page

--- app/src/main/java/cs246/project/itemsNeeded.java
+++ app/src/main/java/cs246/project/itemsNeeded.java
@@ -12,6 +12,14 @@ public class itemsNeeded extends AppCompatActivity {
     // logging tag
     private static final String TAG = "itemsNeeded";
 
+    /**
+     * onCreate
+     *
+     * Method needed to create the activity. Runs other methods to create the buttons on the page
+     *
+     * @author Zach Zendejas
+     * @param savedInstanceState Needed for method
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +30,13 @@ public class itemsNeeded extends AppCompatActivity {
         updateInventoryButton();
     }
 
+    /**
+     * View Inventory Button
+     *
+     * View the inventory button for items needed page.
+     *
+     * @author Zach Zendejas
+     */
     private void viewInventoryButton() {
        Button viewInventory = findViewById(R.id.viewInventory);
        viewInventory.setOnClickListener(new View.OnClickListener(){
@@ -33,6 +48,13 @@ public class itemsNeeded extends AppCompatActivity {
        });
     }
 
+    /**
+     * UpdateInventory Button
+     *
+     * Update Inventory button. Takes user to the Update Inventory page.
+     *
+     * @author Zach Zendejas
+     */
     private void updateInventoryButton(){
        Button updateInventory = findViewById(R.id.updateInventory);
        updateInventory.setOnClickListener(new View.OnClickListener(){
