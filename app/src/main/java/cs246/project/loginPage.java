233ad3f62d48bb6db19a931d16569296000b4318
Ben Perez <per15066@byui.edu>
2019-03-30 23:29:05 -0700

    update to loginpage code

--- app/src/main/java/cs246/project/loginPage.java
+++ app/src/main/java/cs246/project/loginPage.java
@@ -1,5 +1,7 @@
 package cs246.project;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,16 +13,19 @@ public class loginPage extends AppCompatActivity {
     private Button Login;
 
     @Override
-    protected void onCreate(Bundle saved InstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
-
-        Name = (EditText) findViewById(R.id.username);
-        Name = (EditText) findViewById(R.id.password);
-        Login = (Button) findViewById(R.id.login);
     }
 
-    private void validate(String userName, String userPassword){
-        if((userName == "") && (userPassword == ""))
+    private void validate(String userName, String userPassword) {
+        if ((userName.equals("")) && (userPassword.equals(""))){
+            Intent intent = new Intent(loginPage.this, startPage.class);
+            startActivity(intent);
+            }
+        else{
+            Info.setText("Incorrect username and/or password");
+        }
+
     }
 }
