76835a6163d6925ea5b0c7c88f111e4c09a88bfb
Ben Perez <per15066@byui.edu>
2019-04-06 14:31:22 -0700

    the loginPage is now fully functional with validation and takes the user to the start page.

--- app/src/main/java/cs246/project/loginPage.java
+++ app/src/main/java/cs246/project/loginPage.java
@@ -1,36 +1,59 @@
 package cs246.project;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.icu.text.IDNA;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.text.BreakIterator;
+
 public class loginPage extends AppCompatActivity {
 
-    private EditText User;
-    private EditText Password;
-    private Button Login;
-    private Button Signup;
+    EditText username, password;
+    Button login;
+    Button signup;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        username=(EditText)findViewById(R.id.username);
+        password=(EditText)findViewById(R.id.password);
+        login=(Button)findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(login(username.getText().toString(), password.getText().toString())==1)
+                {
+                    Intent intent = new Intent(loginPage.this, startPage.class);
+                    startActivity(intent);
+                }
+            }
+            });
     }
 
     private void showError() {
-        Password.setError("Password and username didn't match");
+        password.setError("Password and username didn't match");
     }
 
-    private void
-    validate(String userName, String userPassword) {
-        if ((userName.equals("")) && (userPassword.equals(""))){
-            Intent intent = new Intent(loginPage.this, startPage.class);
-            startActivity(intent);
+    private int login(String userName, String userPassword) {
+        if (userName.equals("admin") && userPassword.equals("1234"))
+        {
+            return 1;
+
+        }else {
+                showError();
+                return 0;
             }
-        else showError();
+        }
 
-    }
 }
